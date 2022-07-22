639897debfdb5ef992e27487ff720c978e1846c7
Nam Nguyen <namnguyen.8a@gmail.com>
2022-07-22 18:50:12 +0700

    first commit

--- parking_app/consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('vehicles', bootstrap_servers='localhost:29092')
-
-print("Gonna start listening")
-
-for message in consumer:
-    print("Here is all vehicles")
-    print (message)
--- parking_app/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class VehicleCreate(BaseModel):
-    vehicle_name: str
-    vehicle_type: str
-    vehicle_fee: int
